1f81ddcbb83cd83d58ec827e1b4015a90930fdba
adampszczola <adampszczola98@gmail.com>
2020-04-02 12:39:22 +0200

    Dodanie klasy oraz .gitignore

---
+++ src/showVersion.java
@@ -0,0 +1,14 @@
+import java.io.Console;
+public class showVersion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String version = System.getProperty("java.version");
+		String vendor = System.getProperty("java.vendor");
+		System.out.println(vendor);
+		System.out.println(version);
+		
+		
+	}
+
+}
